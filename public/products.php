c73d29a1bf9e19c492e796d32c403e0bb428d718
Julien BENOIT <jbenoit@smag.tech>
2024-04-11 16:44:45 +0200

    twig01

--- public/products.php
+++ public/products.php
@@ -1,7 +1,14 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
-// tu indiques ici à Twig où aller chercher tes fichiers de vues
+
+$products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe'];
+
+// Chargement du chargeur (loader) Twig
 $loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
+
+// Création de l'environnement Twig
 $twig = new Twig\Environment($loader, ['debug' => true]);
 $twig->addExtension(new Twig\Extension\DebugExtension());
+
+echo $twig->render('products.html.twig', ['products' => $products]);
